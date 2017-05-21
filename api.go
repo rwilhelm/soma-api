18a9d85d17a10e4e98fc0f411adc9dba9859669b
René Wilhelm <rene.wilhelm@gmail.com>
2017-05-21 07:13:14 +0200

    Clean API handler

--- api.go
+++ api.go
@@ -1,69 +1,42 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"log"
 	"net/http"
 )
 
-//type apiHandler struct {}
-
-type apiError struct {
-	Tag     string `json:"-"`
-	Error   error  `json:"-"`
-	Message string `json:"error"`
-	Code    int    `json:"code"`
+type Location struct {
+	Latitude  float64 `json:latitude`
+	Longitude float64 `json:longitude`
+	Timestamp float64 `json:timestamp`
 }
 
-type api struct {
-	DB      *sql.DB
-	Handler func(w http.ResponseWriter, r *http.Request, db *sql.DB) *apiError
+type AllLocationsResponse struct {
+	Location []string
 }
 
-func (api apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (a *api) allLocations(w http.ResponseWriter, r *http.Request) {
+	rows, err := a.db.Query("SELECT latitude, longitude, timestamp FROM location")
 
-	// Headers for all responses
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-
-	// Initialize API handler
-	err := api.Handler(w, r, api.DB)
 	if err != nil {
-
-		log.Printf("AAA %s %s %s [%s] %s", r.RemoteAddr, r.Method, r.URL, err.Tag, err.Error)
-
-		w.WriteHeader(err.Code)
-
-		resp := json.NewEncoder(w)
-		err_json := resp.Encode(err)
-		if err_json != nil {
-			log.Println("Encode JSON for error response was failed.")
-			return
-		}
-		return
+		log.Fatal(err)
 	}
 
-	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-}
+	defer rows.Close()
 
-func indexHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) *apiError {
+	for rows.Next() {
 
-	err := db.Ping()
-	if err != nil {
-		return &apiError{
-			"indexHandler db ping",
-			err,
-			"internal server error",
-			http.StatusInternalServerError,
+		var (
+			latitude  float64
+			longitude float64
+			timestamp string
+		)
+
+		err = rows.Scan(&latitude, &longitude, &timestamp)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		w.Write([]byte(timestamp))
 	}
-
-	return nil
-}
-
-func ExampleStripPrefix() {
-	// To serve a directory on disk (/tmp) under an alternate URL path
-	// (/tmpfiles/), use StripPrefix to modify the request URL's path before the
-	// FileServer sees it:
-	http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("/tmp"))))
 }
--- main.go
+++ main.go
@@ -16,30 +16,20 @@ const (
 	PORT   = "3333"
 )
 
-type Location struct {
-	Latitude  float64 `json:latitude`
-	Longitude float64 `json:longitude`
-	Timestamp float64 `json:timestamp`
-}
-
-type Response1 struct {
-	Location []string
-}
-
 type api struct {
 	db *sql.DB
 }
 
 func main() {
-	dbinfo := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", DBUSER, DBNAME, DBPASS)
-	db, err := sql.Open("postgres", dbinfo)
+	dbinfo := fmt.Sprintf(
+		"user=%s dbname=%s password=%s sslmode=disable", DBUSER, DBNAME, DBPASS)
 
+	db, err := sql.Open("postgres", dbinfo)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = db.Ping()
-
 	if err != nil {
 		log.Fatal("[ERROR] Could not establish a connection with the database")
 		log.Fatal(err)
@@ -49,73 +39,15 @@ func main() {
 
 	api := api{db: db}
 
-	http.HandleFunc("/hello", api.handleHello)
-
-	//http.handleFunc("/upload", s.handleUpload)
-	//http.handle
-
-	//hello := helloWorldHandler()
-	//http.Handle("/hello", hello)
-
-	//allLocations := allLocationsHandler()
-	//http.Handle("/api/v2/loc", allLocations)
+	http.HandleFunc("/loc", api.allLocations)
 
 	log.Printf("Listening on :%s", PORT)
 	http.ListenAndServe(":"+PORT, nil)
-
-	//if err != nil && err != http.ErrServerClosed {
-	//	log.Fatal(err)
-	//}
 }
 
-func (a *api) handleHello(w http.ResponseWriter, r *http.Request) {
-	rows, err := a.db.Query("SELECT latitude, longitude, timestamp FROM location")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-
-		var (
-			latitude  float64
-			longitude float64
-			timestamp string
-		)
-
-		err = rows.Scan(&latitude, &longitude, &timestamp)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		w.Write([]byte(timestamp))
-	}
-}
-
-func helloWorldHandler() http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	}
-	return http.HandlerFunc(fn)
-}
-
-//func dbTestHandler() http.Handler {
-//	fn := func(w http.ResponseWriter, r *http.Request, db *sql.DB) http.Handler {
-//		err := db.Ping()
-//		if err != nil {
-//			log.Fatal("DB ERROR")
-//		}
-//		log.Println("DB SUCCESS")
-//		w.Write([]byte("hello world"))
-//	}
-//	return http.HandlerFunc(fn)
-//}
-
-func allLocationsHandler() http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-
-	}
-	return http.HandlerFunc(fn)
+func logger(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s requested %s", r.RemoteAddr, r.URL)
+		h.ServeHTTP(w, r)
+	})
 }
