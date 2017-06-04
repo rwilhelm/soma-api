63cd8fbdacb6200712d3826e1b2883da12e81458
René Wilhelm <rene.wilhelm@gmail.com>
2017-06-04 06:43:50 +0200

    Basic auth (realm)

--- auth.go
+++ auth.go
@@ -1,6 +1,8 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func BasicAuth(pass http.HandlerFunc) http.HandlerFunc {
 
@@ -8,10 +10,14 @@ func BasicAuth(pass http.HandlerFunc) http.HandlerFunc {
 
 		username, password, _ := r.BasicAuth()
 
+		const realm = ""
+
 		if username != "asdf" || password != "bla" {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
 			http.Error(w, "authorization failed", http.StatusUnauthorized)
 			return
 		}
+
 		pass(w, r)
 	}
 }
